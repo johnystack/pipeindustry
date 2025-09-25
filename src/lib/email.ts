5aadda0e34c32c3d1c307bb7f7fd2cdd0e486842
johnystack <120309467+johnystack@users.noreply.github.com>
2025-09-25 18:24:31 +0100

    tooo
    from

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -11,7 +11,7 @@ interface SendEmailProps {
 export async function sendEmail({ to, subject, html }: SendEmailProps) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'pipindustry35@gmail.com',
+      from: 'noreply@pipindustry.org',
       to: [to],
       subject: subject,
       html: html,
